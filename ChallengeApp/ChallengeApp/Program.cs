a4714dee17d4022850494c6a22859fedbf3d8ca8
KL <liberski.k@gmail.com>
2023-10-13 23:36:21 +0200

    Zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 15;
+string sex = "M";
+
+if (name == "Ewa" && age == 33 && sex == "K")
+{
+    Console.WriteLine(name + ", lat " + age);
+}
+else if (sex == "K" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == "M" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna. (Czy " + name + " to imię męskie?)");
+}
+else if (sex != "M" && sex != "K")
+{
+    Console.WriteLine(name + ", proszę określ swoją płeć przez wpisanie do zmiennej płeć (ang. sex) M - dla mężczyzny i K - dla kobiety.");
+}
+else if (true)
+// nie wiedziałem ze VS mi dopisze (true) jako wartość i tym sposobem (przypadkiem) zalatwiłem sobie ostatni ogólny warunek
+{
+    Console.WriteLine("Wymagana większa liczba warunków weryfikacji danych");
+}
